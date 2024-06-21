e43a2219cec5848b0c7c81e067c5939c85f35ba3
GameNight <tmddus9908@naver.com>
2024-06-21 14:57:28 +0900

    move spawnpoint

--- Assets/Planet.cs
+++ Assets/Planet.cs
@@ -5,15 +5,14 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    public Camera upCamera;
 
     private bool _isCrash;
     private void OnCollisionEnter(Collision other)
     {
         if (other != null && _isCrash == false)
         {
-            upCamera.transform.position = new Vector3(0, 0, -50);
             _isCrash = true;
+            BallSpawnPoint.Instance.SetCameraPosition();
         }
     }
 }
